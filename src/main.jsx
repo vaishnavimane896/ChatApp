ff815bc9ef1ebaa3fd7d13ab940d1820052e0e16
Vaishnavi Mane <manevaishnavi635@gmail.com>
2026-03-24 17:55:15 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { AuthContextProvider } from './Context/AuthContext.jsx'
+import { AuthContextProvider } from './context/AuthContext'
 
-createRoot(document.getElementById('root')).render(
-  <AuthContextProvider>
-    <StrictMode>
-    <App />
-  </StrictMode>,
-
-
-  </AuthContextProvider>
-  
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
+  </React.StrictMode>
 )
